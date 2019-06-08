f89b06b564dd8a549885aa479f6eb968a98177f4
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-06-08 19:07:01 +0200

    Add function for date retrieval

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,45 @@
 
 extern crate image;
+extern crate exif;
 
 use image::GenericImageView;
 use image::FilterType::Gaussian;
 use image::jpeg::JPEGEncoder;
-use std::fs::File;
 use image::ColorType;
+
+use std::fs::File;
+
+use exif::Tag;
+use exif::DateTime;
 
 pub fn open_compress_and_save_image(source_path: &str, destination_path: &str) {
     let original = image::open(source_path).unwrap();
-    let resized = original.resize(700, 700, Gaussian);
+    let resized = original.resize(1024, 1024, Gaussian);
     let width = resized.width();
     let height = resized.height();
     let pixels = resized.raw_pixels();
 
     let mut file = File::create(destination_path).unwrap();
-    let mut encoder = JPEGEncoder::new_with_quality(&mut file, 70);
+    let mut encoder = JPEGEncoder::new_with_quality(&mut file, 60);
     encoder.encode(&pixels, width, height, ColorType::RGB(8)).unwrap();
+}
+
+pub fn date_time_string_from_image_path(image_path: &str) -> String {
+    let file = std::fs::File::open(image_path).unwrap();
+    let reader = exif::Reader::new(&mut std::io::BufReader::new(&file)).unwrap();
+    let date_time = reader.get_field(Tag::DateTimeOriginal, false).unwrap();
+
+    return format!("{}", date_time.value.display_as(Tag::DateTimeOriginal));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn date_time_string_is_correct() {
+        let string = date_time_string_from_image_path("tests/test_image.jpg");
+
+        assert_eq!(string.as_str(),"2004-04-09 17:33:15");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@
 use image_mapper;
 
 fn main() {
-    image_mapper::open_compress_and_save_image("windows.jpg", "hej.jpg")    
+    let r = image_mapper::date_time_string_from_image_path("tests/test_image.jpg");
+
+    println!("{}", r.as_str());
 }

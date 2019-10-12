f22515226610732d8475d793400b38ba8bd26019
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-10-12 17:38:13 +0200

    Only compress images if non-test.

--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,5 @@
 
-use std::fs::File;
+use std::fs::{self, File};
 use std::path::Path;
 
 use image::GenericImageView;
@@ -7,6 +7,7 @@ use image::FilterType::Gaussian;
 use image::jpeg::JPEGEncoder;
 use image::ColorType;
 
+#[cfg(not(test))]
 pub fn open_compress_and_save_image(source_path: &Path, destination_path: &Path) {
     let original = image::open(source_path).unwrap();
     let resized = original.resize(1024, 1024, Gaussian);
@@ -17,4 +18,9 @@ pub fn open_compress_and_save_image(source_path: &Path, destination_path: &Path)
     let mut file = File::create(destination_path).unwrap();
     let mut encoder = JPEGEncoder::new_with_quality(&mut file, 60);
     encoder.encode(&pixels, width, height, ColorType::RGB(8)).unwrap();
+}
+
+#[cfg(test)]
+pub fn open_compress_and_save_image(source_path: &Path, destination_path: &Path) {
+    fs::copy(source_path, destination_path).unwrap();
 }

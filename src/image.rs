dc6f67d045f8f13cc514b0b235493f87f4943256
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-10-13 17:52:30 +0200

    Use settings in image compression.

--- src/image.rs
+++ src/image.rs
@@ -12,13 +12,21 @@ use crate::settings::{Settings, ImageQuality};
 #[cfg(not(test))]
 pub fn open_compress_and_save_image(source_path: &Path, destination_path: &Path, settings: &Settings) {
     let original = image::open(source_path).unwrap();
-    let resized = original.resize(1024, 1024, Gaussian);
+    let resized = match settings.image_quality {
+        ImageQuality::Mobile     => original.resize(1024, 1024, Gaussian),
+        ImageQuality::Television => original.resize(1920, 1080, Gaussian),
+    };
+    
     let width = resized.width();
     let height = resized.height();
     let pixels = resized.raw_pixels();
 
     let mut file = File::create(destination_path).unwrap();
-    let mut encoder = JPEGEncoder::new_with_quality(&mut file, 60);
+    let factor = match settings.image_quality {
+        ImageQuality::Mobile     => 60, // TODO: tune these constants
+        ImageQuality::Television => 60,
+    };
+    let mut encoder = JPEGEncoder::new_with_quality(&mut file, factor);
     encoder.encode(&pixels, width, height, ColorType::RGB(8)).unwrap();
 }
 

d4a8e8925a6b4effe530018c5d560ae845b414cb
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-10-19 18:18:28 +0200

    Handle orientation when converting images.

--- src/image.rs
+++ src/image.rs
@@ -7,6 +7,7 @@ use image::GenericImageView;
 use image::FilterType::Gaussian;
 use image::jpeg::JPEGEncoder;
 use image::ColorType;
+use image::DynamicImage;
 use unwrap::unwrap;
 use exif::{Reader, Tag, Value};
 
@@ -14,24 +15,16 @@ use crate::settings::{Settings, ImageQuality};
 
 #[cfg(not(test))]
 pub fn open_compress_and_save_image(source_path: &Path, destination_path: &Path, settings: &Settings) {
-    let original = unwrap!(image::open(source_path), "Could not open the image {:?}", source_path);
-    let resized = match settings.image_quality {
-        ImageQuality::Mobile     => original.resize(1024, 1024, Gaussian),
-        ImageQuality::Television => original.resize(1920, 1080, Gaussian),
-    };
-        
-    let color = resized.color();
-    let width = resized.width();
-    let height = resized.height();
-    let pixels = resized.raw_pixels();
+    let original = read_original_image(source_path);
+    let orientation = orientation_from_path(source_path);
+    let rotated = rotate_image(original, orientation);
+    let dimensions = dimensions_from_settings(settings);
+    let resized = resize_image(rotated, dimensions);
+    encode_and_save_image(resized, destination_path, settings);
+}
 
-    let mut file = unwrap!(File::create(destination_path), "Could not create the image {:?}", destination_path);
-    let factor = match settings.image_quality {
-        ImageQuality::Mobile     => 30,
-        ImageQuality::Television => 70,
-    };
-    let mut encoder = JPEGEncoder::new_with_quality(&mut file, factor);
-    unwrap!(encoder.encode(&pixels, width, height, color), "Could not encode the image {:?}", destination_path);
+fn read_original_image(image_path: &Path) -> DynamicImage {
+    unwrap!(image::open(image_path), "Could not open the image {:?}", image_path)
 }
 
 fn orientation_from_path(image_path: &Path) -> u16 {
@@ -50,6 +43,44 @@ fn orientation_from_path(image_path: &Path) -> u16 {
     }
 
     1
+}
+
+fn rotate_image(image: DynamicImage, orientation: u16) -> DynamicImage {
+    match orientation {
+        1 => image,
+        6 => image.rotate90(),
+        8 => image.rotate270(),
+        3 => image.rotate180(),
+        _ => panic!("Unsupported orientation")
+    }
+}
+
+fn dimensions_from_settings(settings: &Settings) -> (u32, u32) {
+    match settings.image_quality {
+        ImageQuality::Mobile => (1024, 1024),
+        ImageQuality::Television => (1920, 1080),
+    }
+}
+
+fn resize_image(image: DynamicImage, dimensions: (u32, u32)) -> DynamicImage {
+    let (width, height) = dimensions;
+    image.resize(width, height, Gaussian)
+}
+
+fn encode_and_save_image(image: DynamicImage, destination_path: &Path, settings: &Settings) {
+    let color = image.color();
+    let width = image.width();
+    let height = image.height();
+    let pixels = image.raw_pixels();
+
+    let mut file = unwrap!(File::create(destination_path), "Could not create the image {:?}", destination_path);
+    let factor = match settings.image_quality {
+        ImageQuality::Mobile     => 30,
+        ImageQuality::Television => 70,
+    };
+
+    let mut encoder = JPEGEncoder::new_with_quality(&mut file, factor);
+    unwrap!(encoder.encode(&pixels, width, height, color), "Could not encode the image {:?}", destination_path);
 }
 
 #[cfg(test)]

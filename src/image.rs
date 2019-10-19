6cadda62e4f745c41f3f5d9c993214e277c7983c
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-10-19 17:55:41 +0200

    Add orientation checking from exif.

--- src/image.rs
+++ src/image.rs
@@ -1,12 +1,14 @@
 
 use std::fs::File;
 use std::path::Path;
+use std::io::BufReader;
 
 use image::GenericImageView;
 use image::FilterType::Gaussian;
 use image::jpeg::JPEGEncoder;
 use image::ColorType;
 use unwrap::unwrap;
+use exif::{Reader, Tag, Value};
 
 use crate::settings::{Settings, ImageQuality};
 
@@ -17,7 +19,7 @@ pub fn open_compress_and_save_image(source_path: &Path, destination_path: &Path,
         ImageQuality::Mobile     => original.resize(1024, 1024, Gaussian),
         ImageQuality::Television => original.resize(1920, 1080, Gaussian),
     };
-    
+        
     let color = resized.color();
     let width = resized.width();
     let height = resized.height();
@@ -30,6 +32,24 @@ pub fn open_compress_and_save_image(source_path: &Path, destination_path: &Path,
     };
     let mut encoder = JPEGEncoder::new_with_quality(&mut file, factor);
     unwrap!(encoder.encode(&pixels, width, height, color), "Could not encode the image {:?}", destination_path);
+}
+
+fn orientation_from_path(image_path: &Path) -> u16 {
+    let file = unwrap!(File::open(image_path), "Could not open the image for exif {:?}", image_path);
+    let mut buf_reader = BufReader::new(&file);
+    let exif_reader = Reader::new(&mut buf_reader);
+
+    if let Ok(exif_reader) = exif_reader {
+        if let Some(orientation) = exif_reader.get_field(Tag::Orientation, false) {
+            if let Value::Short(orientation) = &orientation.value {
+                if orientation.len() == 1 {
+                    return orientation[0];
+                }
+            }
+        }
+    }
+
+    1
 }
 
 #[cfg(test)]

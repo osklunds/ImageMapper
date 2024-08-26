6b5dff0934689bcad16708b5f69010341a8a57a3
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-26 11:02:38 +0200

    Move helpers to EOF

--- src/mapper/tests.rs
+++ src/mapper/tests.rs
@@ -41,114 +41,6 @@ fn test_ensure_path_is_directory_does_not_remove_directory() {
     assert!(destination_file.exists());
 }
 
-fn create_src_structure_in_dir(path: &Path) {
-    create_dir1_in_dir(path);
-    create_dir2_in_dir(path);
-
-    create_dir_with_name_in_dir("dir3", path);
-    create_small_image_with_exif_in_dir(path);
-
-    let small_without_exif_jpg_path =
-        path.join("small-without-exif.jpg");
-    fs::copy("test_resources/small-without-exif.jpg", small_without_exif_jpg_path).unwrap();
-
-    let small_without_exif_png_path =
-        path.join("small-without-exif.png");
-    fs::copy("test_resources/small-without-exif.png", small_without_exif_png_path).unwrap();
-
-    let text_file_path = path.join("text_file.txt");
-    File::create(text_file_path).unwrap();
-
-    let word_path = path.join("word.docx");
-    File::create(word_path).unwrap();
-
-    let movie_path = path.join("video.m4v");
-    File::create(movie_path).unwrap();
-
-    let exp_dir_items = vec![
-        "dir1",
-        "dir1/small-with-exif.jpg",
-        "dir2",
-        "dir2/subdir1",
-        "dir2/subdir1/small-with-exif.jpg",
-        "dir2/subdir2",
-        "dir2/small-with-exif.jpg",
-        "dir3",
-        "small-with-exif.jpg",
-        "small-without-exif.jpg",
-        "small-without-exif.png",
-        "text_file.txt",
-        "video.m4v",
-        "word.docx",
-    ];
-    assert_dir_items(&exp_dir_items, path);
-}
-
-fn create_dir1_in_dir(dir_path: &Path) {
-    let dir1_path = create_dir_with_name_in_dir("dir1", dir_path);
-    create_small_image_with_exif_in_dir(&dir1_path);
-}
-
-fn create_dir_with_name_in_dir(name: &str, dir_path: &Path) -> PathBuf {
-    let subdir_path = dir_path.join(name);
-    fs::create_dir(&subdir_path).unwrap();
-    subdir_path
-}
-
-fn create_small_image_with_exif_in_dir(dir_path: &Path) {
-    let image_path = dir_path.join("small-with-exif.jpg");
-    fs::copy("test_resources/small-with-exif.jpg", image_path).unwrap();
-}
-
-fn create_dir2_in_dir(dir_path: &Path) {
-    let dir2_path = create_dir_with_name_in_dir("dir2", dir_path);
-
-    create_subdir1_in_dir(&dir2_path);
-    create_subdir2_in_dir(&dir2_path);
-    create_small_image_with_exif_in_dir(&dir2_path);
-}
-
-fn create_subdir1_in_dir(dir_path: &Path) {
-    let subdir1_path = create_dir_with_name_in_dir("subdir1", dir_path);
-    create_small_image_with_exif_in_dir(&subdir1_path);
-}
-
-fn create_subdir2_in_dir(dir_path: &Path) {
-    create_dir_with_name_in_dir("subdir2", dir_path);
-}
-
-const SETTINGS: MapperSettings = MapperSettings {
-    app_settings: Settings {
-        image_quality: ImageQuality::Mobile,
-        verbose_print: false,
-        include_videos: true,
-    },
-    open_compress_and_save_image: no_convert_image
-};
-
-const SETTINGS_NO_VIDEO: MapperSettings = MapperSettings {
-    app_settings: Settings {
-        image_quality: ImageQuality::Mobile,
-        verbose_print: false,
-        include_videos: false,
-    },
-    open_compress_and_save_image: no_convert_image
-};
-
-pub fn no_convert_image(
-    source_path: &Path,
-    destination_path: &Path,
-    _settings: &Settings,
-) -> bool {
-    unwrap!(
-        fs::copy(source_path, destination_path),
-        "Could not copy from \"{}\" to \"{}\"",
-        source_path.display(),
-        destination_path.display()
-    );
-    true
-}
-
 #[test]
 fn test_map_directory_correctly_fills_empty_dst_with_videos() {
     let src_dir = tempfile::tempdir().unwrap();
@@ -482,3 +374,116 @@ fn test_map_directory_with_image_conversion() {
 
     check_that_dst_structure_is_correct_if_videos(dst_path, true);
 }
+
+// -----------------------------------------------------------------------------
+// Helpers
+// -----------------------------------------------------------------------------
+
+fn create_src_structure_in_dir(path: &Path) {
+    create_dir1_in_dir(path);
+    create_dir2_in_dir(path);
+
+    create_dir_with_name_in_dir("dir3", path);
+    create_small_image_with_exif_in_dir(path);
+
+    let small_without_exif_jpg_path =
+        path.join("small-without-exif.jpg");
+    fs::copy("test_resources/small-without-exif.jpg", small_without_exif_jpg_path).unwrap();
+
+    let small_without_exif_png_path =
+        path.join("small-without-exif.png");
+    fs::copy("test_resources/small-without-exif.png", small_without_exif_png_path).unwrap();
+
+    let text_file_path = path.join("text_file.txt");
+    File::create(text_file_path).unwrap();
+
+    let word_path = path.join("word.docx");
+    File::create(word_path).unwrap();
+
+    let movie_path = path.join("video.m4v");
+    File::create(movie_path).unwrap();
+
+    let exp_dir_items = vec![
+        "dir1",
+        "dir1/small-with-exif.jpg",
+        "dir2",
+        "dir2/subdir1",
+        "dir2/subdir1/small-with-exif.jpg",
+        "dir2/subdir2",
+        "dir2/small-with-exif.jpg",
+        "dir3",
+        "small-with-exif.jpg",
+        "small-without-exif.jpg",
+        "small-without-exif.png",
+        "text_file.txt",
+        "video.m4v",
+        "word.docx",
+    ];
+    assert_dir_items(&exp_dir_items, path);
+}
+
+fn create_dir1_in_dir(dir_path: &Path) {
+    let dir1_path = create_dir_with_name_in_dir("dir1", dir_path);
+    create_small_image_with_exif_in_dir(&dir1_path);
+}
+
+fn create_dir_with_name_in_dir(name: &str, dir_path: &Path) -> PathBuf {
+    let subdir_path = dir_path.join(name);
+    fs::create_dir(&subdir_path).unwrap();
+    subdir_path
+}
+
+fn create_small_image_with_exif_in_dir(dir_path: &Path) {
+    let image_path = dir_path.join("small-with-exif.jpg");
+    fs::copy("test_resources/small-with-exif.jpg", image_path).unwrap();
+}
+
+fn create_dir2_in_dir(dir_path: &Path) {
+    let dir2_path = create_dir_with_name_in_dir("dir2", dir_path);
+
+    create_subdir1_in_dir(&dir2_path);
+    create_subdir2_in_dir(&dir2_path);
+    create_small_image_with_exif_in_dir(&dir2_path);
+}
+
+fn create_subdir1_in_dir(dir_path: &Path) {
+    let subdir1_path = create_dir_with_name_in_dir("subdir1", dir_path);
+    create_small_image_with_exif_in_dir(&subdir1_path);
+}
+
+fn create_subdir2_in_dir(dir_path: &Path) {
+    create_dir_with_name_in_dir("subdir2", dir_path);
+}
+
+const SETTINGS: MapperSettings = MapperSettings {
+    app_settings: Settings {
+        image_quality: ImageQuality::Mobile,
+        verbose_print: false,
+        include_videos: true,
+    },
+    open_compress_and_save_image: no_convert_image
+};
+
+const SETTINGS_NO_VIDEO: MapperSettings = MapperSettings {
+    app_settings: Settings {
+        image_quality: ImageQuality::Mobile,
+        verbose_print: false,
+        include_videos: false,
+    },
+    open_compress_and_save_image: no_convert_image
+};
+
+pub fn no_convert_image(
+    source_path: &Path,
+    destination_path: &Path,
+    _settings: &Settings,
+) -> bool {
+    unwrap!(
+        fs::copy(source_path, destination_path),
+        "Could not copy from \"{}\" to \"{}\"",
+        source_path.display(),
+        destination_path.display()
+    );
+    true
+}
+

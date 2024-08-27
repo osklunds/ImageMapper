03c43a4f08b623e454de7e65b5661e79bfb3aebf
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-27 17:31:22 +0200

    Add error messages

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,19 @@ fn main() {
     match result {
         Ok(_) => {}
         Err(MapperError::SrcDoesNotExist) => {
-            println!("{:?}", "hej");
+            println!("Error: The specified source directory '{}' does not exist or is not a directory", source_path.display());
         }
         Err(MapperError::DstDoesNotExist) => {
-            println!("{:?}", "hej");
+            println!("Error: The specified destination directory '{}' does not exist or is not a directory", destination_path.display());
         }
         Err(MapperError::SrcInsideDst) => {
-            println!("{:?}", "hej");
+            println!("Error: The specified source '{}' lies inside the specified destination directory '{}'", source_path.display(), destination_path.display());
         }
         Err(MapperError::DstInsideSrc) => {
-            println!("{:?}", "hej");
+            println!("Error: The specified destination '{}' lies inside the specified source directory '{}'", destination_path.display(), source_path.display());
         }
-        Err(MapperError::DstTopLevelEntryNotInSrc(_missing_path)) => {
-            println!("{:?}", "hej");
+        Err(MapperError::DstTopLevelEntryNotInSrc(missing_path)) => {
+            println!("Error: The entry '{}' exists as a top-level entry in the specified destination directory, but it does NOT exist in the specified source directory. Running the program like this would cause it to be deleted, so as a safety precaution, the program stops here, because this error might indicate that an incorrect destination directory was specified. Proceding might cause many files to be deleted by mistake. Double check the destination directory and delete the entry manually instead. Note, this check is only done for top-level entries, NOT for entries inside sub directories.", missing_path.display());
         }
     }
 }
